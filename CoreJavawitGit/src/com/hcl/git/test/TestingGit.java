8f02e5216fbb2ca2c2ecb5464fb254a56d70e8a4
sravan <neelisravan.aws@gmail.com>
2018-02-24 21:41:14 +0530

    Comitted the project with commetns are added

---
+++ CoreJavawitGit/src/com/hcl/git/test/TestingGit.java
@@ -0,0 +1,12 @@
+package com.hcl.git.test;
+
+public class TestingGit {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//Testing Git repo
+		System.out.println("Welcome to Git Code sharing tool");
+
+	}
+
+}
