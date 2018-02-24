dd7db49d81ea5428dfca3eb8b494448afd80e253
Expert <Expert@Sravan>
2018-02-24 22:00:45 +0530

    New class with array of string names

---
+++ CoreJavawitGit/src/com/hcl/git/test/TestingConflicts.java
@@ -0,0 +1,15 @@
+package com.hcl.git.test;
+
+public class TestingConflicts {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String[] names= {"Ram","parvathama","prabhu","suresh"};
+		for(int i=0;i<names.length;i++) {
+		System.out.format("Hello \n",names[i]);
+
+	}
+	}
+
+}
