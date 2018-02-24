95793f3fd47ffbd768969a568ffb9711db6adb36
Expert <Expert@Sravan>
2018-02-24 22:31:14 +0530

    Added some more String literals to array

--- CoreJavawitGit/src/com/hcl/git/test/TestingConflicts.java
+++ CoreJavawitGit/src/com/hcl/git/test/TestingConflicts.java
@@ -5,7 +5,7 @@ public class TestingConflicts {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		String[] names= {"Ram","parvathama","prabhu","suresh"};
+		String[] names= {"Ram","parvathama","prabhu","suresh","Sravan","Haritha","Heman"};
 		for(int i=0;i<names.length;i++) {
 		System.out.format("Hello \n",names[i]);
 
